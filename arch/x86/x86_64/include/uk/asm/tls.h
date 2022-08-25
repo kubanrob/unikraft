kubanrob/unikraft

commit 5988db03a3af54ded8b9b2295e09cd585c6807c3

arch/x86_64: increase TLS alignment
Some CPU instructions require the operand to be aligned on a 16-byte
or 32-byte boundary. Such operations may involved thread-local
variables. When it is the case, the compiler (and linker) will increase
the alignment requirements for the `.tdata` and `.tbss` sections in the
ELF objects, and in TLS program header.

As Unikraft `uksched` TLS handling currently ignores alignment
requirements set in the ELF image, this commit increases the constant
alignment from 8 to 32, to allow for all use cases.

Signed-off-by: Cyril Soldani <cyril.soldani@uliege.be>
Reviewed-by: Sergiu Moga <sergiu.moga@protonmail.com>
Approved-by: Vlad-Andrei Badoiu <vlad_andrei.badoiu@upb.ro>
Tested-by: Unikraft CI <monkey@unikraft.io>
GitHub-Pull-Request: #287

arch/x86/x86_64/include/uk/asm/tls.h
@@ -50,7 +50,10 @@ static inline __sz ukarch_tls_area_size(void)
 
 static inline __sz ukarch_tls_area_align(void)
 {
-	return 8;
+	/* The minimal required TLS area alignment should ideally come from the
+	 * ELF header, but 32 should be fine for all use X86_64 instructions.
+	 */
+	return 32;
 }
 
 static inline void ukarch_tls_area_copy(void *tls_area)

plat/common/include/uk/plat/common/common.lds.h
@@ -103,7 +103,7 @@
 	uk_inittab_end = .;
 
 #define TLS_SECTIONS							\
-	. = ALIGN(0x8);							\
+	. = ALIGN(0x20);						\
 	_tls_start = .;							\
 	.tdata :							\
 	{								\
@@ -117,7 +117,7 @@
 		*(.tbss)						\
 		*(.tbss.*)						\
 		*(.gnu.linkonce.tb.*)					\
-		. = ALIGN(0x8);						\
+		. = ALIGN(0x20);					\
 	}								\
 	_tls_end = . + SIZEOF(.tbss);
 

commit 5e7f28de692cd0a1732aba710ef7e440e046a48b

arch/libcontext: Move TLS code to libcontext (x86) and fix allocation problem (caused by wrong padding by commit 5988db0 / PR #287)
Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

arch/x86/Makefile.uk
@@ -8,3 +8,4 @@ $(eval $(call addlib,libcontext))
 LIBCONTEXT_SRCS-y += $(LIBCONTEXT_BASE)/ctx.c|isr
 LIBCONTEXT_SRCS-y += $(LIBCONTEXT_BASE)/ectx.c|isr
 LIBCONTEXT_SRCS-$(CONFIG_ARCH_X86_64) += $(LIBCONTEXT_BASE)/x86_64/ctx.S|x86_64
+LIBCONTEXT_SRCS-$(CONFIG_ARCH_X86_64) += $(LIBCONTEXT_BASE)/x86_64/tls.c|x86_64

arch/x86/x86_64/Makefile.uk
@@ -45,6 +45,3 @@ ARCHFLAGS-$(CONFIG_MARCH_X86_64_BTVER1)         += -march=btver1
 ISR_ARCHFLAGS-$(CONFIG_MARCH_X86_64_BTVER1)     += -march=btver1
 ARCHFLAGS-$(CONFIG_MARCH_X86_64_BTVER2)         += -march=btver2
 ISR_ARCHFLAGS-$(CONFIG_MARCH_X86_64_BTVER2)     += -march=btver2
-
-$(eval $(call addlib,libx86_64arch))
-LIBX86_64ARCH_SRCS-y += $(CONFIG_UK_BASE)/arch/x86/x86_64/tls.c

arch/x86/x86_64/include/uk/asm/tls.h
@@ -1,74 +1,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
-/*
- * Authors: Florian Schmidt <florian.schmidt@neclab.eu>
- *
- * Copyright (c) 2019, NEC Europe Ltd., NEC Corporation. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-#ifndef __UKARCH_TLS_H__
-#error Do not include this header directly
-#endif
-
-#include <uk/arch/types.h>
-#include <string.h>
-
-extern char _tls_start[], _etdata[], _tls_end[];
-
-static inline __sz ukarch_tls_area_size(void)
-{
-	/* x86_64 ABI requires that fs:%0 contains the address of itself, to
-	 * allow certain optimizations. Hence, the overall size of the size of
-	 * the TLS area, plus 8 bytes.
-	 */
-	return _tls_end - _tls_start + 8;
-}
-
-static inline __sz ukarch_tls_area_align(void)
-{
-	/* The minimal required TLS area alignment should ideally come from the
-	 * ELF header, but 32 should be fine for all use X86_64 instructions.
-	 */
-	return 32;
-}
-
-static inline void ukarch_tls_area_copy(void *tls_area)
-{
-	__sz tls_len = _tls_end - _tls_start;
-	__sz tls_data_len = _etdata - _tls_start;
-	__sz tls_bss_len = _tls_end - _etdata;
-
-	memcpy(tls_area, _tls_start, tls_data_len);
-	memset(tls_area + tls_data_len, 0, tls_bss_len);
-	/* x86_64 ABI requires that fs:%0 contains the address of itself. */
-	*((__uptr *)(tls_area + tls_len)) = (__uptr)(tls_area + tls_len);
-}
-
-static inline __uptr ukarch_tls_pointer(void *tls_area)
-{
-	return (__uptr) tls_area + ((__uptr) _tls_end - (__uptr) _tls_start);
-}

arch/x86/x86_64/tls.c
@@ -1,8 +1,13 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Authors: Cyril Soldani <cyril.soldani@uliege.be>
+ * Authors: Florian Schmidt <florian.schmidt@neclab.eu>
+ *          Cyril Soldani <cyril.soldani@uliege.be>
+ *          Simon Kuenzer <simon@unikraft.io>
  *
+ * Copyright (c) 2019, NEC Europe Ltd., NEC Corporation. All rights reserved.
  * Copyright (c) 2021, University of Liège. All rights reserved.
+ * Copyright (c) 2022, NEC Laboratories Europe GmbH, NEC Corporation.
+ *                     All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,12 +34,118 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#ifndef CONFIG_LIBCONTEXT_CLEAR_TBSS
+#define CONFIG_LIBCONTEXT_CLEAR_TBSS 1
+#endif /* CONFIG_LIBCONTEXT_CLEAR_TBSS */
 
-/* Dummy thread-local variable to force a non-empty .tbss section. Padding is
- * automatically added to the .tbss section by the linker script to align the
- * thread pointer which directly follows the TLS area. However, ld's generated
- * offsets for TLS variables are wrong when the .tbss section only contains
- * padding and no actual variables. This definition ensures there will be at
- * least one.
+#include <string.h>
+#include <uk/print.h>
+#include <uk/hexdump.h>
+#include <uk/assert.h>
+#include <uk/essentials.h>
+#include <uk/arch/types.h>
+
+extern char _tls_start[], _etdata[], _tls_end[];
+
+/*
+ * This file implements a static exec TLS layout
+ * (variant 2 with one static TLS block).
+ *
+ * aligned --> +-------------------------+ \
+ * allocation  |                         | |
+ *             | .tdata                  | |
+ *             |                         | |
+ *             +-------------------------+ |
+ *             |                         | |
+ *             | .tbss                   |  > tls_area
+ *             |                         | |
+ *             +-------------------------+ |
+ *             | / PADDING / / / / / / / | |
+ *             +-------------------------+ |
+ * tlsp -------+-> self pointer (void *) | |
+ *             +-------------------------+ /
+ *
+ * Partly derived from
+ *  https://wiki.osdev.org/Thread_Local_Storage
+ *  (Feb 2022)
  */
-__thread int _uk_dummy_tls;
+
+__sz ukarch_tls_area_align(void)
+{
+	/* NOTE: The minimal required TLS area alignment should be ideally read
+	 *       from the ELF program header. Unfortunately, we can't read it
+	 *       because we do not have them and the auxiliary vector populated
+	 *       when booting as guest. 32 should be safe enough to take, since
+	 *       it should cover all X86_64 instruction requirements.
+	 * NOTE: This alignment must be bigger or equal to 8 bytes
+	 *       (sizeof(void *)).
+	 */
+	return 0x20;
+}
+
+__sz ukarch_tls_area_size(void)
+{
+	/* NOTE: X86_64 ABI requires that fs:%0 contains the address of itself,
+	 *       to allow certain optimizations. Hence, the overall size of an
+	 *       TLS allocation is the aligned up TLS area plus 8 bytes for this
+	 *       self-pointer.
+	 */
+	__sz tls_len =  ALIGN_UP((__uptr) _tls_end - (__uptr) _tls_start,
+				   sizeof(void *));
+	return tls_len + sizeof(void *);
+}
+
+__uptr ukarch_tls_tlsp(void *tls_area)
+{
+	UK_ASSERT(IS_ALIGNED((__uptr) tls_area, ukarch_tls_area_align()));
+
+	return (__uptr) tls_area + ukarch_tls_area_size() - sizeof(void *);
+}
+
+void *ukarch_tls_area_get(__uptr tlsp)
+{
+	__uptr tls_area;
+
+	tls_area = tlsp - (ukarch_tls_area_size() - sizeof(void *));
+
+	UK_ASSERT(IS_ALIGNED((__uptr) tls_area, ukarch_tls_area_align()));
+
+	return (void *) tls_area;
+}
+
+void ukarch_tls_area_init(void *tls_area)
+{
+	const __sz tdata_len = (__uptr) _etdata  - (__uptr) _tls_start;
+	const __sz tbss_len  = (__uptr) _tls_end - (__uptr) _etdata;
+	const __sz padding   = ukarch_tls_area_size() - (tbss_len + tdata_len
+							 + sizeof(void *));
+
+	UK_ASSERT(IS_ALIGNED((__uptr) tls_area, ukarch_tls_area_align()));
+
+	uk_pr_debug("tls_area_init: target: %p (%"__PRIsz" bytes)\n",
+		    tls_area, ukarch_tls_area_size());
+	uk_pr_debug("tls_area_init: copy (.tdata): %"__PRIsz" bytes\n",
+		    tdata_len);
+	uk_pr_debug("tls_area_init: uninitialized (.tbss): %"__PRIsz" bytes\n",
+		    (__sz) tbss_len);
+	uk_pr_debug("tls_area_init: pad: %"__PRIsz" bytes\n",
+		    padding);
+	uk_pr_debug("tls_area_init: self ptr: %p\n",
+		    (void *) ukarch_tls_pointer(tls_area));
+
+	/* .tdata */
+	memcpy((void *)((__uptr) tls_area),
+	       _tls_start, tdata_len);
+
+#if CONFIG_LIBCONTEXT_CLEAR_TBSS
+	/* clear .tbss and padding */
+	memset((void *)((__uptr) tls_area + tdata_len),
+	       0x0, tbss_len + padding);
+#endif /* CONFIG_LIBCONTEXT_CLEAR_TBSS */
+
+	/* x86_64 ABI requires that fs:%0 contains the address of itself. */
+	*((__uptr *) ukarch_tls_tlsp(tls_area))
+		= (__uptr) ukarch_tls_tlsp(tls_area);
+
+	uk_hexdumpCd(tls_area, ukarch_tls_area_size());
+}

include/uk/arch/tls.h
@@ -1,8 +1,11 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
  * Authors: Florian Schmidt <florian.schmidt@neclab.eu>
+ *          Simon Kuenzer <simon.kuenzer@neclab.eu>
  *
  * Copyright (c) 2019, NEC Europe Ltd., NEC Corporation. All rights reserved.
+ * Copyright (c) 2022, NEC Laboratories Europe GmbH, NEC Corporation.
+ *                     All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,6 +36,49 @@
 #ifndef __UKARCH_TLS_H__
 #define __UKARCH_TLS_H__
 
-#include <uk/asm/tls.h>
+#include <uk/arch/types.h>
+
+/**
+ * Returns the alignement requirement for an allocation
+ * to be used as TLS
+ *
+ * @return
+ *  Alignment in bytes
+ */
+__sz ukarch_tls_area_align(void);
+
+/**
+ * Returns the required size for an allocation to be used
+ * as TLS
+ *
+ * @return
+ *  TLS area size in bytes
+ */
+__sz ukarch_tls_area_size(void);
+
+/**
+ * Returns the TLS pointer (tlsp) that is used to activate
+ * the TLS memory `tls_area` with `ukplat_tlsp_set()`
+ *
+ * @param tls_area
+ *  TLS area to activate
+ * @return
+ *  TLS pointer that can be used with `ukplat_tlsp_set()`
+ */
+__uptr ukarch_tls_tlsp(void *tls_area);
+
+/**
+ * Derive the TLS area from a given TLS architecture pointer
+ */
+void *ukarch_tls_area_get(__uptr tlsp);
+
+/**
+ * Initializes/resets a memory area for TLS use based
+ * on the TLS template.
+ *
+ * @param tls_area
+ *  TLS area to initialize
+ */
+void ukarch_tls_area_init(void *tls_area);
 
 #endif /* __UKARCH_TLS_H__ */

plat/common/include/uk/plat/common/common.lds.h
@@ -124,8 +124,15 @@
 		*(.tbss)						\
 		*(.tbss.*)						\
 		*(.gnu.linkonce.tb.*)					\
-		. = ALIGN(0x20);					\
+		*(.tcommon)						\
 	}								\
+	/*								\
+	 * NOTE: Because the .tbss section is zero-sized in the final	\
+	 *       ELF image, just setting _tls_end to the end of it	\
+	 *       does not give us the the size of the memory area once	\
+	 *       loaded, so we use SIZEOF to have it point to the end.	\
+	 *       _tls_end is only used to compute the .tbss size.	\
+	 */								\
 	_tls_end = . + SIZEOF(.tbss);
 
 #define DATA_SECTIONS							\